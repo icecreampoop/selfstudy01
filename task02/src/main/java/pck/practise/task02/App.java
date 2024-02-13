bca1cf11a18b5162dddc5f3d7718938b84de8b2c
icecreampoop <pchiku@hotmail.sg>
2024-02-13 21:25:00 +0800

    creating files

---
+++ task02/src/main/java/pck/practise/task02/App.java
@@ -0,0 +1,13 @@
+package pck.practise.task02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
